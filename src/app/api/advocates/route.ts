1ff5f2966dc19f5e6d4f8216c7bb210b7d0e78de
marielklem <47639519+marielklem@users.noreply.github.com>
2025-08-14 22:28:51 -0400

    update endpoint to take query strings and use postgres full text search

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,43 @@
+import { NextResponse } from "next/server";
 import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { advocates } from "@/db/schema";
+import { sql } from "drizzle-orm";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function GET(req: Request) {
 
-  const data = advocateData;
+  try {
+    const { searchParams } = new URL(req.url);
+    const query = searchParams.get("query");
 
-  return Response.json({ data });
+    //TOOD: some kind of handling if no query sent in. Based on location maybe?
+
+    const results = await db.execute(
+      sql`
+        SELECT *,
+          ts_rank(
+            to_tsvector('english',
+              coalesce(first_name, '') || ' ' ||
+              coalesce(last_name, '') || ' ' ||
+              coalesce(city, '') || ' ' ||
+              coalesce(degree, '')
+            ),
+            plainto_tsquery('english', ${query})
+          ) AS rank
+        FROM ${advocates}
+        WHERE to_tsvector('english',
+            coalesce(first_name, '') || ' ' ||
+            coalesce(last_name, '') || ' ' ||
+            coalesce(city, '') || ' ' ||
+            coalesce(degree, '')
+          )
+          @@ plainto_tsquery('english', ${query})
+        ORDER BY rank DESC;
+      `
+    );
+
+    return Response.json(results);
+  } catch (e) {
+    console.log(e);
+    return Response.json({ error: "Search failed" }, { status: 500 });
+  }
 }
